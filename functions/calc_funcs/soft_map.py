01f382d98493030736e1718a778218ad0f36c8c3
Jerry WSL <jerryshum2636@gmail.com>
2025-08-25 19:42:22 -0400

    fix: update dealer_map values and enhance soft_map tests

--- functions/calc_funcs/soft_map.py
+++ functions/calc_funcs/soft_map.py
@@ -34,20 +34,21 @@ def soft_map(total_soft, total_dealer):
     }
     
     dealer_map = {
-        1: 10,
-        2: 1,
-        3: 2,
-        4: 3,
-        5: 4,
-        6: 5,
-        7: 6,
-        8: 7,
-        9: 8,
-        10: 9,
+        1: 9,
+        2: 0,
+        3: 1,
+        4: 2,
+        5: 3,
+        6: 4,
+        7: 5,
+        8: 6,
+        9: 7,
+        10: 8,
     }
     
     
     chart_index = soft_total_map[total_soft]
     dealer_index = dealer_map[total_dealer]
+    
     
     return soft_hands_strategy_chart[chart_index][dealer_index]
